cc913f38ae0d8899d25b771259bcdc16e14d0727
vaqsii <vaqsii23@gmail.com>
2025-03-01 19:32:06 +0400

    Initial commit

--- app/(auth)/sign-in/credentialsForm.tsx
+++ app/(auth)/sign-in/credentialsForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+
 import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 import React from 'react'
